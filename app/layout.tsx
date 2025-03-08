c0f1a869bcea56c490ff295f4d4c8cec85f5bd56
Pettem Akhil Varsh <raisaarmahadevan@gmail.com>
2025-03-09 03:02:37 +0530

    add: SpeedInsights

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from 'next';
 import './globals.css';
 import CosmicCursor from './components/CosmicCursor';
 import { Analytics } from '@vercel/analytics/next';
+import { SpeedInsights } from "@vercel/speed-insights/next"
 
 export const metadata: Metadata = {
   title: 'AK\'s Galaxy',
@@ -19,7 +20,7 @@ export default function RootLayout({
         <CosmicCursor />
         {children}
         <Analytics />
-
+        <SpeedInsights />
       </body>
     </html>
   );
