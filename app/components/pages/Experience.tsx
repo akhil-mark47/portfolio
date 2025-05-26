985a07bce2fa1b3c735bbe4b2fa12e39d8f21162
Akhil Varsh Pettem <148253964+akhil-mark47@users.noreply.github.com>
2025-05-26 22:00:01 +0530

    Update Experience.tsx

--- app/components/pages/Experience.tsx
+++ app/components/pages/Experience.tsx
@@ -5,8 +5,8 @@ export default function Experience() {
     <GlassSection id="experience" title="Experience">
                 <div className="space-y-6">
                   <div className="gradient-border p-4">
-                    <h3 className="text-xl font-bold text-gradient">Web Developer (Freelance)</h3>
-                    <p className="font-[JetBrains Mono] text-gray-400">Navaidix Solutions, U.S Remote | Jan 2025 - Present</p>
+                    <h3 className="text-xl font-bold text-gradient">Freelancer</h3>
+                    <p className="font-[JetBrains Mono] text-gray-400">Hyderabad,Telangana(Remote) | Jan 2025 - May 2025</p>
                     <p>Building a dynamic staff recruitment website using React to enhance candidate hiring experience.</p>
                   </div>
                   <div className="gradient-border p-4">
