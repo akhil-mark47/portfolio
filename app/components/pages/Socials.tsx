d8ff9baa8b2381ac9c9273aa938b2f2c607962b1
akhil-varsh <raisaarmahadevan@gmail.com>
2026-09-13 17:33:09 +0530

    Fix typo in Twitter link for Socials component

--- app/components/pages/Socials.tsx
+++ app/components/pages/Socials.tsx
@@ -47,7 +47,7 @@ const socialLinks = [
     hoverRotate: -5
   },
   { 
-    href: "https://x.com/akhilvarsp",
+    href: "https://x.com/akhilvarshp",
     Icon: FaXTwitter,
     style: { color: "#000" },
     hoverRotate: -5
